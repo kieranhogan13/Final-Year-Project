ddc8ca554587775e5f94aee25126f95edc110d4c
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-20 13:46:50 +0000

    Version 2.2 - Connecting to Azure DB

---
+++ Unity/Assets/Scripts/DataLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLoader : MonoBehaviour {
+
+    public string[] items;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+        WWW itemsData = new WWW("http://fypc12561353.cloudapp.net/testConn.php");
+        yield return itemsData;
+        string itemsDataString = itemsData.text;
+        print(itemsDataString);
+        items = itemsDataString.Split(';');
+        print(GetDataValue(items[0], "Name:"));
+    }
+	
+    string GetDataValue(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        return value;
+    }
+}
