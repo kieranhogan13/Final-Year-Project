b460f1e722c0cf20b488431bf856b031801621f9
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-22 23:32:00 +0000

    Version 2.6 - Tutorial buttons and fixing scale issue

--- Unity/Assets/Scripts/LevelManager.cs
+++ Unity/Assets/Scripts/LevelManager.cs
@@ -5,6 +5,27 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
 
     public int score;
+    public GameObject[] buttons;
+    public Camera cam;
+
+    void Awake()
+    {
+        
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+
+        if (PlayerPrefs.HasKey("bgR"))
+        {
+            Color color0 = new Color(PlayerPrefs.GetFloat("bgR"), PlayerPrefs.GetFloat("bgG"), PlayerPrefs.GetFloat("bgB"), 1);
+            cam.backgroundColor = color0;
+        }
+        if (PlayerPrefs.HasKey("btX"))
+        {
+            foreach (GameObject button in buttons)
+            {
+                button.transform.localScale = new Vector3(PlayerPrefs.GetFloat("btX"), PlayerPrefs.GetFloat("btY"), 1);
+            }
+        }
+    }
 
     public void AddPoints(int points)
     {
