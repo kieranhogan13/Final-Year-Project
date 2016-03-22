9fe306a14643517362681161df4549fb2f2097fd
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-22 00:00:35 +0000

    Version 2.5 - Fixed tracking

--- Unity/Assets/Scripts/Guess.cs
+++ Unity/Assets/Scripts/Guess.cs
@@ -12,11 +12,20 @@ public class Guess : MonoBehaviour {
     public Canvas MainCanvas, WinCanvas, LoseCanvas;
     public AudioClip welldone;
     private AudioSource source;
+
     public bool word1, word2, word3, word4, word5;
-    public int applause, wrongWordCount;
+    public int applause, wrongWordCount, scoreSaved;
+
+    public string levelname, category;
+    public float timeplayed;
+    public int score;
+    public string[] top10Scores;
+    public string db_url = "http://fypc12561353.cloudapp.net/";
 
     void Start()
     {
+        levelname = "Level 5 - Test";
+        category = "Test";
         MainCanvas.enabled = true;
         WinCanvas.enabled = false;
         LoseCanvas.enabled = false;
@@ -28,6 +37,7 @@ public class Guess : MonoBehaviour {
         word5 = false;
         wrongWordCount = 0;
         applause = 0;
+        scoreSaved = 0;
     }
 
     void Awake()
@@ -57,10 +67,34 @@ public class Guess : MonoBehaviour {
         {
             CorrectAnswer();
         }
-        if (wrongWordCount >= 2)
+        if (wrongWordCount >= 3)
         {
             WrongAnswer();
         }
+    }
+
+    IEnumerator SaveScores()
+    {
+        // first we create a new WWWForm, that means a "post" command goes out to our database (for futher information just google "post" and "get" commands for html/php
+        WWWForm form = new WWWForm();
+
+        // with this line we will give a new name and save our score into that name
+        // those "" indicate a string and attach the score after the comma to it
+        form.AddField("newLevel", levelname);
+        form.AddField("newCategory", category);
+        form.AddField("newTime", timeplayed.ToString());
+        form.AddField("newScore", score);
+
+        print("debug: test");
+        // the next line will start our php file that saves the Score and attaches the saved values from the "form" to it
+        // For this tutorial I've used a new variable "db_url" that stores the path
+        WWW webRequest = new WWW(db_url + "save.php", form);
+
+        // with this line we'll wait until we get an info back
+        yield return webRequest;
+
+        string webRequestString = webRequest.text;
+        print(webRequestString);
     }
 
     void CorrectAnswer()
@@ -68,10 +102,21 @@ public class Guess : MonoBehaviour {
         MainCanvas.enabled = false;
         WinCanvas.enabled = true;
         PlayerPrefs.SetInt("LevelProgress", 1);
+        PlayerPrefs.SetFloat("LeveTime", Time.timeSinceLevelLoad);
+
         if (applause == 0)
         {
             source.PlayOneShot(welldone, 1.0f);
             applause ++;
+        }
+
+        timeplayed = Time.timeSinceLevelLoad;
+        score = 1;
+
+        if (scoreSaved == 0)
+        {
+            scoreSaved = 1;
+            StartCoroutine(SaveScores());
         }
     }
 
@@ -80,7 +125,16 @@ public class Guess : MonoBehaviour {
         MainCanvas.enabled = false;
         LoseCanvas.enabled = true;
         PlayerPrefs.SetInt("LevelProgress", 0);
-        
+        PlayerPrefs.SetFloat("LeveTime", Time.timeSinceLevelLoad);
+
+        timeplayed = Time.timeSinceLevelLoad;
+        score = 0;
+
+        if (scoreSaved == 0)
+        {
+            scoreSaved = 1;
+            StartCoroutine(SaveScores());
+        }
     }
 
     public void Word1Button()
