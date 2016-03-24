18df0f8037c021b7b8fa5651b40288787558a760
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-24 00:13:23 +0000

    Version 2.8 - Working on more scenes

---
+++ Unity/Assets/Scripts/BGButtonSizeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGButtonSizeController : MonoBehaviour {
+
+    public GameObject[] buttons;
+    public Camera cam;
+    
+    // Use this for initialization
+    void Start () {
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+
+        if (PlayerPrefs.HasKey("bgR"))
+        {
+            Color color0 = new Color(PlayerPrefs.GetFloat("bgR"), PlayerPrefs.GetFloat("bgG"), PlayerPrefs.GetFloat("bgB"), 1);
+            cam.backgroundColor = color0;
+        }
+        if (PlayerPrefs.HasKey("btX"))
+        {
+            foreach (GameObject button in buttons)
+            {
+                button.transform.localScale = new Vector3(PlayerPrefs.GetFloat("btX"), PlayerPrefs.GetFloat("btY"), 1);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Unity/Assets/Scripts/Menu.cs
+++ Unity/Assets/Scripts/Menu.cs
@@ -8,8 +8,7 @@ public class Menu : MonoBehaviour {
 
     public Canvas MainCanvas;
     public bool CanMute;
-    public GameObject[] buttons;
-    public Camera cam;
+
     public bool firstTime;
 
     void  Start()
@@ -34,22 +33,8 @@ public class Menu : MonoBehaviour {
     void Awake()
     {    
         MainCanvas.enabled = true;
-
         CanMute = true;
-        buttons = GameObject.FindGameObjectsWithTag("Button");
-
-        if (PlayerPrefs.HasKey("bgR"))
-        {
-            Color color0 = new Color(PlayerPrefs.GetFloat("bgR"), PlayerPrefs.GetFloat("bgG"), PlayerPrefs.GetFloat("bgB"), 1);
-            cam.backgroundColor = color0;
-        }
-        if (PlayerPrefs.HasKey("btX"))
-        {
-            foreach (GameObject button in buttons)
-            {
-                button.transform.localScale = new Vector3(PlayerPrefs.GetFloat("btX"), PlayerPrefs.GetFloat("btY"), 1);
-            }
-        }
+        
     }
 
     public void Mute()
