d9067dc747b678d5ace060e895fffe72e10df1f4
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-24 01:04:07 +0000

    Version 2.8 - Added sound for buttons, plus fixed resize buttons

---
+++ Unity/Assets/Scripts/NPC.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+    public GameObject speechBubble;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            speechBubble.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            speechBubble.SetActive(false);
+        }
+    }
+}
