e5877c798a3c1210c9cab767f0117490ebce6706
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-24 17:09:10 +0000

    Version 2.9 - Set up camera tracking, bg and more advanced scenes

---
+++ Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    public float xMax, yMax, xMin, yMin;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = GameObject.Find("Dog").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+	}
+}
