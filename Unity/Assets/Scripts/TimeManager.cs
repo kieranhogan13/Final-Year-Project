d17ae89c0fc77d9fd6238a3f343036f539f100de
Kieran Hogan <kieranhogan13@gmail.com>
2016-03-26 20:52:33 +0000

    Version 3.1 - Added time bar

---
+++ Unity/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimeManager : MonoBehaviour {
+
+    public Image TimeBar;
+    public float DecreaseAmount;
+	
+	// Update is called once per frame
+	void Update () {
+        TimeBar.fillAmount -= DecreaseAmount = Time.deltaTime/10;
+
+        if (TimeBar.fillAmount == 0f)
+        {
+            print("Game Over");
+            this.enabled = false;
+        }
+	}
+}
